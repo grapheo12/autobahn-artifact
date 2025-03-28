c69fc5087b2943f1383701b1a5340287edb38e80
Shubham Mishra <grapheo12@gmail.com>
2025-03-27 20:26:57 -0700

    Semi open loop

--- node/src/benchmark_client.rs
+++ node/src/benchmark_client.rs
@@ -67,7 +67,7 @@ async fn main() -> Result<()> {
     info!("Transactions size: {} B", size);
 
     // NOTE: This log entry is used to compute performance.
-    let rate = 100;
+    let rate = 1000;
     info!("Transactions rate: {} tx/s", rate);
 
     info!("Number of clients: {}", clients);
