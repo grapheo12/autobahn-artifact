8b172a8507b75defea217770e338c53f0ad17d0a
Shubham Mishra <grapheo12@gmail.com>
2025-03-29 20:26:21 -0700

    null blocks autobahn

--- primary/src/core.rs
+++ primary/src/core.rs
@@ -1856,7 +1856,7 @@ impl Core {
 
                 // Garbage collect (can be ascyn)
                 //self.clean_slot(sl);
-                self.clean_slot_periods(sl);
+                self.clean_slot_periods(sl).await;
             }
             _ => {}
         }
